09e809eb75daebf6d6c0978de7253dee0d477d03
Damien <damienambient@gmail.com>
2025-04-18 17:42:09 +0300

    LogoSection finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import LogoSection from "./components/LogoSection";
 import NavBar from "./components/NavBar";
 import Hero from "./sections/hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
@@ -8,6 +9,7 @@ const App = () => {
       <NavBar></NavBar>
       <Hero></Hero>
       <ShowcaseSection></ShowcaseSection>
+      <LogoSection></LogoSection>
     </>
   );
 };
