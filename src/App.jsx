91fbb8443ba53306c326020a4d9447a3f50f6f02
Damien <damienambient@gmail.com>
2025-04-19 15:21:51 +0300

    experience layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from "./components/NavBar";
 import FeatureCards from "./sections/FeatureCards";
 import Hero from "./sections/hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
+import ExperienceSection from "./sections/ExperienceSection";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <ShowcaseSection></ShowcaseSection>
       <LogoSection></LogoSection>
       <FeatureCards></FeatureCards>
+      <ExperienceSection></ExperienceSection>
     </>
   );
 };
